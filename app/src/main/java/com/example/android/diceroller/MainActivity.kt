8d396f63156fea48aa906788d1c3c322f22d1a46
cesar acosta <ceagdev@gmail.com>
2021-06-02 01:16:51 -0400

    Importar los view, vincularlos con el ui y agregar funcion para Tap el boton.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        val resultTextView: TextView = findViewById(R.id.textView)
+
+        rollButton.setOnClickListener{
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            resultTextView.text="6"
+        }
+
     }
 }
