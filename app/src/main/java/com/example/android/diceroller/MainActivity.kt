736fc40dc43bd321a6fe4a3359afe37fc79d56b4
cesar acosta <ceagdev@gmail.com>
2021-06-02 01:49:41 -0400

    Agregar un segundo Dado Agregar Comentarios

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -6,10 +6,16 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Join the UI button to add a onClick Listener to Roll the Dice
         val rollButton: Button = findViewById(R.id.button)
 
         rollButton.setOnClickListener{
@@ -20,11 +26,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRolled = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text= "$diceRolled"
+        // Create 2 new Dice object with 6 sides and roll it
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+
+        val diceRolled1 = dice1.roll()
+        val diceRolled2 = dice2.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView1: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView3)
+
+        resultTextView1.text= "$diceRolled1"
+        resultTextView2.text= "$diceRolled2"
     }
 }
 
