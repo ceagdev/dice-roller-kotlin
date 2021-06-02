fc87ca16168402cb5519e48a23c64a632a73d071
cesar acosta <ceagdev@gmail.com>
2021-06-02 17:29:31 -0400

    Agregar Imagenes de Dados y cambiarlas con el numero correspondiente

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 /**
@@ -24,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             rollDice()
         }
 
+        // Do a dice roll when the app starts
+        rollDice()
+
     }
 
     /**
@@ -33,17 +36,25 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create 2 new Dice object with 6 sides and roll it
         val dice1 = Dice(6)
-        val dice2 = Dice(6)
-
         val diceRolled1 = dice1.roll()
-        val diceRolled2 = dice2.roll()
 
         // Update the screen with the dice roll
-        val resultTextView1: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView3)
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when(diceRolled1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
 
-        resultTextView1.text= "$diceRolled1"
-        resultTextView2.text= "$diceRolled2"
+        // Update the content description
+        diceImage.contentDescription = diceRolled1.toString()
     }
 }
 
