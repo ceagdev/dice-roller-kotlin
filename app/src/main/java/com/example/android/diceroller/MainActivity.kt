d44d66115e31e8ef1a333ccf134dbea28aa88656
cesar acosta <ceagdev@gmail.com>
2021-06-02 01:40:29 -0400

    Agregar la Clase Dice y asociar el la funcion rollDice para que ejecute el roll

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -11,13 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        val resultTextView: TextView = findViewById(R.id.textView)
 
         rollButton.setOnClickListener{
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
-            resultTextView.text="6"
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRolled = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text= "$diceRolled"
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
